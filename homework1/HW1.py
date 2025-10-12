f184363767551098ed423060220ad21141b58b2f
Marek <novotny@embedix.eu>
2025-10-12 12:39:38 +0200

    update 1.1

--- homework1/HW1.py
+++ homework1/HW1.py
@@ -26,6 +26,65 @@ def clock(widht):
 
 clock(11)
 
+def middle(height, width):
+    if width < 3 or width> 20:
+        return 1
+    elif width % 2 == 0:
+        return 1
+    if height > width:
+        return 2
+    i = 3
+    k = width % 2 * 2
+    l = int(width / 2)
+    u = int(width / 2)
+    o = 0
+    print( l * " " , "o")
+
+    while i  <= width + 2:
+
+        print( l * " " +i * "@")
+        i +=2
+        k -= 1
+        l -= 1
+    p = 1
+    while p <= height:
+        print("@"+ width * "x"+ "@" )
+        p += 1
+    while width >= 1:
+        print( o* " "+  (width + 2) * "@")
+        o += 1
+        width -= 2
+
+    print( u * " ", "o")
+
+middle(3, 9)
 
 
+def house(width, height,  character):
 
+    if width < 3 or width> 20:
+        return 1
+    elif width % 2 == 0:
+        return 1
+    if height > width:
+        return 2
+    if len(character) == 1 and not character.isalpha() and not character.islower():
+        return 3
+
+    i = 3
+    k = width % 2 * 2
+    l = int(width / 2)
+    u = int(width / 2)
+    o = 0
+    print(l * " ", "^")
+
+    while i <= width :
+        print(l * " " + i * "^")
+        i += 2
+        k -= 1
+        l -= 1
+
+    while o < height:
+        print(" |" + (width - 2) * character + "|")
+        o += 1
+house(5, 3, "k")
